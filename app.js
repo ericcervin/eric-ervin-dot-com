d78c8581711bdb4be51515559a8c8d65982134ed
ericcervin <ericcervin@gmail.com>
2018-02-21 00:16:48 -0500

    added lodash dependency and serialism resource

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const Mustache = require('mustache');
 const logger = require('morgan');
 const app = express();
 
+const serialHTML = require('./serialism');
+
 const rootTemplate = `
  <!DOCTYPE html>
   <html lang="en">
@@ -23,6 +25,14 @@ const rootTemplate = `
   <p><a href="https://github.com/ericcervin/eric-ervin-dot-com">https://github.com/ericcervin/eric-ervin-dot-com</a></p>
   <br>
   </div>
+  <div id="resources">
+  <table>
+  <thead><tr><th scope="col">Resource</th><th scope="col">Description</th><th scope="col">Data Updated</th></tr></thead>
+  <tbody>
+  <tr><td><a href="/serialism">Serialism</a></td><td>Toying with set theory and dodecaphony</td><td>N/A</td></tr>
+  </tbody>
+  </table>
+  </div>
   </body>
   </html>
 
@@ -32,6 +42,7 @@ const rootTemplate = `
 app.use(logger("short"));
 
 app.get('/',(req,res) => {res.send(Mustache.render(rootTemplate))});
+app.get('/serialism',(req,res) => (res.send(serialHTML())));
 
 app.use(function(req,res) {res.status(404).send("<!DOCTYPE html><html lang=\"en\"><head><title>404</title></head><body><p><strong>404 not found</strong></body></html>")});
 
---
+++ serialism.js
@@ -0,0 +1,64 @@
+const _ = require('lodash');
+const Mustache = require('mustache');
+
+const serialTemplate = `<!DOCTYPE html>
+                  <html lang=\"en\">
+                 <head>  
+                 <style> table,th,td {
+                      border: 1px solid black;
+                      border-collapse: collapse;
+                      padding: 3px;
+                      text-align: center;}
+                               
+                 td {text-align: left}
+                 </style>
+	             <title>Serialism</title>
+                 </head>
+                 <body>
+                 <table>
+                 <tbody>
+                 <tr><th>p0</th>{{#p0}}<td>{{.}}</td>{{/p0}}</tr>
+				 <tr><th>r0</th>{{#r0}}<td>{{.}}</td>{{/r0}}</tr>
+				 <tr><th>i0</th>{{#i0}}<td>{{.}}</td>{{/i0}}</tr>
+				 <tr><th>ri0</th>{{#ri0}}<td>{{.}}</td>{{/ri0}}</tr>
+                 </tbody>
+                 </table>
+                 </body>
+                 </html>`
+	  
+function randomDodecaRow() {return _.shuffle(_.range(12))};
+
+function absolutePitchClass(cl)
+                 {
+			     if (cl < 0) {cl += 12};
+				 return cl;
+				 }
+				 
+const retrograde = (rw) => {
+	row =  rw.map((x) => x);
+	return _.reverse(row)}
+
+function shiftToZero(rw){
+     const dst = rw[0];
+     let row = rw.map((x) => x - dst);
+	 row = row.map((x) => absolutePitchClass(x));
+	 return row;
+}
+
+function invert(cl){
+	if (cl !== 0) {cl = 12 - cl};
+	return cl
+}
+
+function invertRow (rw) {return _.map(rw,invert)};
+
+function serialObject() {
+	const p0 = shiftToZero(randomDodecaRow());
+    const r0 = retrograde(p0);
+    const i0 = invertRow(p0);
+    const ri0 = invertRow(r0);
+    return {p0: p0, r0: r0, i0: i0, ri0: ri0}
+}
+function serialHTML(){return Mustache.render(serialTemplate,serialObject())};
+
+module.exports = serialHTML;
