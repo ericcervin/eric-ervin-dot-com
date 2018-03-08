b28790dd1c9e9e82386e2e27ee50649ba01748da
ericcervin <ericcervin@gmail.com>
2018-03-07 20:08:16 -0500

    starting destiny resource

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ const logger = require('morgan');
 const app = express();
 
 const root = require('./routes/root');
+const destiny = require('./routes/destiny');
 const serialism = require('./routes/serialism');
 const powerball = require('./routes/powerball');
 
 app.use(logger("short"));
 
 app.use('/',root);
+app.use('/destiny',destiny);
 app.use('/powerball',powerball);
 app.use('/serialism',serialism);
 app.use(function(req,res) {res.status(404).send("<!DOCTYPE html><html lang=\"en\"><head><title>404</title></head><body><p><strong>404 not found</strong></body></html>")});
