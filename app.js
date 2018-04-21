a0203b6b0d0f1f497d1c529b6109593bd6ef04ee
ericcervin <ericcervin@gmail.com>
2018-04-21 18:43:28 -0400

    updated 404 text to match other sites

--- app.js
+++ app.js
@@ -19,7 +19,6 @@ app.use(function(req,res) {res.status(404).send(`<!DOCTYPE html>
               <head>
               <title>Error 404 Not Found</title>
               </head>
-              <body>404 - Not Found</body>
-              </html>`)});
+              <body>404 - Not Found</body></html>`)});
 
 app.listen(80,() => console.log("Listening to 80"));
